5564d6f9cb2e215c6a499be15e5908a1d4e4903f
MichalPem <m.pemcak@gmail.com>
2021-11-01 09:38:10 +0100

    Initial commit

---
+++ backend/src/main/java/cz/mipemco/lnbitsbank/Application.java
@@ -0,0 +1,17 @@
+package cz.mipemco.lnbitsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    /**
+     * Application's main method
+     *
+     * @param args the console arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
